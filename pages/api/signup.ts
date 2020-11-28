e3d43738bd2fe7fa33b669ad804a62c8be02959c
moneyan9 <m.tanimura1226@gmail.com>
2020-11-28 17:27:08 +0900

    tutorial24 finished

---
+++ pages/api/signup.ts
@@ -0,0 +1,48 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+import { hash } from 'bcrypt';
+
+
+export default async function signup(req: NextApiRequest, res: NextApiResponse) {
+
+    //DBを開く
+    const db = await open(
+        {
+            filename: './mydb.sqlite',
+            driver: sqlite3.Database
+        }
+    );
+
+    //POSTをうける
+    if (req.method === 'POST') {
+
+        //bcryptのhashで
+        hash(req.body.password,
+            10,
+            async function (err, hash) {
+                //insert文を発行
+                const statement = await db.prepare(
+                    'INSERT INTO Person (name, email, password) values (?,?,?)'
+                );
+
+                const result = await statement.run(
+                    req.body.name,
+                    req.body.email,
+                    hash  //ハッシュ化された値を格納
+                );
+
+
+                //デバッグ用に出力（実際はセキュリティのためpasswordカラムはとらない）
+                const person = await db.all('select * from person');
+                res.json(person);
+            }
+        );
+    } else {
+        res.status(405).json(
+            { message: 'We Only Support POST method!' }
+        );
+    }
+
+
+}
